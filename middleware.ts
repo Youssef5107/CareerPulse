5c8843cf883a1e7b1fd77c7eb4bd915662029546
Youssef5107 <ybelalmarzouk@gmail.com>
2026-09-18 12:56:10 +0300

    fix: update middleware to export default auth function

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
+import type { NextRequest } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth;
+export const middleware = auth;
 
 export const config = {
   matcher: ["/jobseeker/:path*", "/employer/:path*"],
